0c44e4cb075911a90bd431d8c82d26472ad37840
Alex Huang <alexh95@mit.edu>
2019-08-20 23:25:37 -0700

    Get basic reminding functionality working with delay
    - Switched to pendulum timing library to make date/time handling much
    easier
    - Use simple sleep between calls to reduce CPU load

--- iris.py
+++ iris.py
@@ -1,5 +1,6 @@
 import pickle
-import datetime
+import time
+import pendulum as pd
 import os.path
 
 from config import config, rules
@@ -22,7 +23,7 @@ tw = Client(TW_SID, TW_TOKEN)
 #        return [False for rule in rules]
 
 
-def alert(nuber, message):
+def alert(number, message):
     '''
     Helper method to send an alert via SMS or email
     listing: dictionary of availability date and price of listing
@@ -34,23 +35,50 @@ def alert(nuber, message):
         to = number
     )
 
-def get_messages(sender):
+def get_messages(sender, start, end):
     '''
     Gets all incoming messages in the database
     sender: the origin number SMS is delivered from
     '''
     messages = []
-    today = datetime.datetime.now()
-    for sms in tw.messages.list(limit=5, from_=sender, date_sent_after = datetime.datetime(today.year, today.month, today.day + 1, 0,0), date_sent_before = datetime.datetime(today.year, today.month, today.day + 1, 12, 0)):
+    smses = tw.messages.list(limit=5, from_=sender, date_sent_after = start, date_sent_before = end)
+    print('Found', len(smses), 'mesages, sent on:')
+    for sms in smses:
         print(sms.date_sent)
         messages.append(sms)
     return messages
+
+def run_rule(rule):
+    now = pd.now().add(days=1)
+    start_time, end_time = rule['start_time'], rule['end_time']
+    # TODO: figure out if these times need to be in UTC time zone
+    start = now.replace(hour = start_time.hour, minute = start_time.minute, second = start_time.second).in_timezone('UTC')
+    end = now.replace(hour = end_time.hour, minute = end_time.minute, second = end_time.second).in_timezone('UTC')
+    numbers = rule['numbers']
+    message = rule['message']
+    delay = rule['delay']
+    delay_count = rule.setdefault('delay_count', 0)
+
+    if delay_count == 0:
+        rule['delay_count'] = (now - start).in_minutes() // delay + 1
+
+    print(delay_count, now, start.add(minutes=delay_count * delay).in_timezone('America/Los_Angeles'))
+
+    if now >= start.add(minutes=delay_count * delay).in_timezone('America/Los_Angeles') and now <= end:
+        print(delay_count, now, start.add(minutes=delay_count * delay).in_timezone('America/Los_Angeles'))
+        for number in numbers:
+            messages = get_messages(number, start, end)
+            if len(messages) < 1:
+                print('Alerting', number, 'with message:', message)
+                rule['delay_count'] = rule['delay_count'] + 1
+                print(rule)
+                #alert(number, message)
+            else:
+                # TODO: apply a timestamp so we can skip checking messages that already have a response 
+                rule['delay_count'] = 0
         
 if __name__ == "__main__":
-    for rule in rules:
-        numbers = rule['numbers']
-        for number in numbers:
-            messages = get_messages(number)
-            if len(messages) < 1:
-                print('alerting')
-                alert(number, rule['message'])
+    while True:
+        for rule in rules:
+            run_rule(rule)
+        time.sleep(10)
